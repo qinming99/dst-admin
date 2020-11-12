eebfca07851c98e09316ed0606293abaa2baf308
dzzhyk <1354839386@qq.com>
2020-11-12 18:57:07 +0800

    更新CentOS依赖于 install.sh

--- src/main/resources/shell/install.sh
+++ src/main/resources/shell/install.sh
@@ -25,12 +25,13 @@ create() {
 # 配置环境
 main() {
     # mac
-    if (( "$sys" == "Darwin" )); then
+    if [ "$sys" == "Darwin" ]; then
         echo "${project} - 操作系统：MacOS"
         mkdir ~/steamcmd && cd ~/steamcmd
         wget ${darwin_steamcmd_link}
         tar -zxvf steamcmd_osx.tar.gz
         ./steamcmd.sh +login anonymous +force_install_dir ~/dst +app_update 343050 validate +quit
+        cd ~/dst
         mkdir ~/dst/bin
         echo ~/dst/dontstarve_dedicated_server_nullrenderer.app/Contents/MacOS/dontstarve_dedicated_server_nullrenderer -console -cluster MyDediServer -shard Master > ~/dst/bin/overworld.sh
         echo ~/dst/dontstarve_dedicated_server_nullrenderer.app/Contents/MacOS/dontstarve_dedicated_server_nullrenderer -console -cluster MyDediServer -shard Caves > ~/dst/bin/cave.sh
@@ -43,11 +44,11 @@ main() {
     fi
 
     # linux
-    if (( "$sys" == "Linux" )); then
+    if [ "$sys" == "Linux" ]; then
         distribution=$(lsb_release -i | awk '{print $3}')   # 发行版本
         echo "${project} - 操作系统：Linux"
         echo "${project} - ${distribution} ${machine}"
-        if (( "$machine" == "x86_64" )); then
+        if [ "$machine" == "x86_64" ]; then
             case $distribution in
                 CentOS)
                     echo "${project} - 安装CentOS依赖环境"
@@ -55,8 +56,10 @@ main() {
                     cd ~/steamcmd
                     wget ${linux_steamcmd_link}
                     tar -xvzf steamcmd_linux.tar.gz
-                    sudo yum install -y glibc.i686 libstdc++.i686
-                    sudo yum install -y screen
+                    sudo yum install -y glibc.i686 libstdc++.i686 ncurses-libs.i686 screen libcurl.i686
+                    sudo yum install -y SDL2.x86_64 SDL2_gfx-devel.x86_64 SDL2_image-devel.x86_64 SDL2_ttf-devel.x86_64
+                    # CentOS需要建立libcurl-gnutls.so.4软连接
+                    ln -s /usr/lib/libcurl.so.4 /usr/lib/libcurl-gnutls.so.4
                     create
                 ;;
                 Ubuntu)
