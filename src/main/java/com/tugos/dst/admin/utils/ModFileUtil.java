da7939c5c66cce4f7c957d1bad5836dbc00797d9
qinming <1275737636@qq.com>
2022-05-30 19:55:30 +0800

    fix：修复有些MOD设置解析mod编号错误的bug

--- src/main/java/com/tugos/dst/admin/utils/ModFileUtil.java
+++ src/main/java/com/tugos/dst/admin/utils/ModFileUtil.java
@@ -1,12 +1,14 @@
 package com.tugos.dst.admin.utils;
 
-import com.google.common.base.Splitter;
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.ReUtil;
+import org.apache.commons.collections.CollectionUtils;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * @author qinming
@@ -22,28 +24,39 @@ public class ModFileUtil {
      * @return 编号列表
      */
     public static List<String> readModConfigFile(String path) {
-        List<String> list = new ArrayList<>();
-        String regEx = "[^0-9]";
-        Pattern p = Pattern.compile(regEx);
-        try (FileReader fr = new FileReader(path);
-             BufferedReader br = new BufferedReader(fr)) {
-            String tmp;
-            while ((tmp = br.readLine()) != null) {
-                if (tmp.contains("workshop-")) {
-                    List<String> split = Splitter.on("=").splitToList(tmp);
-                    split.forEach(e -> {
-                        if (e.contains("workshop-")) {
-                            Matcher matcher = p.matcher(e);
-                            list.add(matcher.replaceAll("").trim());
-                        }
-                    });
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+        File file = new File(path);
+        if (file.exists()){
+            String content = FileUtil.readString(path, "utf-8");
+            return findModNo(content);
+        }else {
+            return new ArrayList<>();
         }
-        return list;
     }
+
+
+    /**
+     * mod匹配正则
+     */
+    public static final String MOD_REGEX = "\"workshop-\\w[-\\w+]*\"";
+
+    /**
+     * 获取modNo
+     *
+     * @param content mod信息
+     */
+    public static List<String> findModNo(String content) {
+        List<String> result = new ArrayList<>();
+        List<String> resultFindAll = ReUtil.findAll(MOD_REGEX, content, 0, new ArrayList<>());
+        //匹配出来的结果："workshop-1651623054"
+        if (CollectionUtils.isNotEmpty(resultFindAll)) {
+            resultFindAll.forEach(e -> {
+                String modNo = e.replace("\"", "").split("-")[1];
+                result.add(modNo);
+            });
+        }
+        return result;
+    }
+
 
     /**
      * 写入mod
